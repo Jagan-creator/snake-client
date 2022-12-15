7ac389defc172aeb2bf725060fcadba64e07b89e
Joel Hagan <joel.hagan.8@gmail.com>
2022-12-14 19:45:26 -0700

    added a function to handle specific user input so that it terminates the running client

--- play.js
+++ play.js
@@ -3,3 +3,22 @@ const { connect } = require('./client');
 
 console.log("Connecting ...");
 connect();
+
+// setup interface to handle user input form their keyboard inputs
+
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on("data", handleUserInput);        // done so here
+  return stdin;
+};
+
+// function for user input is below
+
+const handleUserInput = function() {
+  if (key === '\u0003') {
+    process.exit();
+  }
+};
