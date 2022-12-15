3e8ec1d58bdaa20d2c37104981e13bf33fcbf10f
Joel Hagan <joel.hagan.8@gmail.com>
2022-12-14 19:52:27 -0700

    used ES6 syntax for requiring the setupInput function and also moved our user input functions to an input.js file

--- play.js
+++ play.js
@@ -1,24 +1,5 @@
-const net = require("net");
 const { connect } = require('./client');
+const { setupInput } = require("./input");
 
 console.log("Connecting ...");
 connect();
-
-// setup interface to handle user input form their keyboard inputs
-
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-  stdin.on("data", handleUserInput);        // done so here
-  return stdin;
-};
-
-// function for user input is below
-
-const handleUserInput = function() {
-  if (key === '\u0003') {
-    process.exit();
-  }
-};
